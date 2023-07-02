ac7c697518798e4447ede9ed0a0e2c156d2096ea
Diego Macrini <macrini@proximify.ca>
2023-07-02 14:32:33 -0400

    fix

--- src/webpackHelper.js
+++ src/webpackHelper.js
@@ -29,7 +29,7 @@ function validUrl(url) {
     return href.endsWith('/') ? href.slice(0, -1) : href;
 }
 
-function getWebpackPlugins(federateModuleName) {
+function getWebpackPlugins(federateModuleName, exposes, tunnelUrl) {
     return [
         new ModuleFederationPlugin({
             name: federateModuleName,
@@ -70,22 +70,22 @@ function getWebpackPlugins(federateModuleName) {
         //     minRatio: 0.8,
         //     deleteOriginalAssets: false,
         // }),
-        function () {
-            this.hooks.done.tap('BuildCompletePlugin', (stats) => {
-                if (stats.compilation.errors.length === 0) {
-                    // console.log('Webpack build completed successfully!');
-                    const separator = '-'.repeat(40); // Dashed line separator
-                    const message =
-                        chalk.green.bold('Tunnel: ') + chalk.white(`${TUNNEL_URL}/${module}`);
+        // function () {
+        //     this.hooks.done.tap('BuildCompletePlugin', (stats) => {
+        //         if (stats.compilation.errors.length === 0) {
+        //             // console.log('Webpack build completed successfully!');
+        //             const separator = '-'.repeat(40); // Dashed line separator
+        //             const message =
+        //                 chalk.green.bold('Tunnel: ') + chalk.white(`${tunnelUrl}/${module}`);
 
-                    console.log('\n' + separator);
-                    console.log(message);
-                    console.log(separator + '\n');
-                } else {
-                    console.log('Webpack build encountered errors.');
-                }
-            });
-        },
+        //             console.log('\n' + separator);
+        //             console.log(message);
+        //             console.log(separator + '\n');
+        //         } else {
+        //             console.log('Webpack build encountered errors.');
+        //         }
+        //     });
+        // },
     ];
 }
 
@@ -379,7 +379,7 @@ module.exports = function (argv, rootDir) {
                 },
             ],
         },
-        plugins: getWebpackPlugins(federateModuleName),
+        plugins: getWebpackPlugins(federateModuleName, exposes, TUNNEL_URL),
         watchOptions: {
             ignored: ['**/node_modules'],
         },
